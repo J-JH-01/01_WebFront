5d5f067cb801756024e9c7a2316892c54ec53bb4
J-JH-01 <winter55555@naver.com>
2024-08-29 17:47:39 +0900

    24.8.29 수정본

--- 3_JAVASCRIPT/solveIt/메뉴판관리.js
+++ 3_JAVASCRIPT/solveIt/메뉴판관리.js
@@ -22,4 +22,50 @@ document.getElementById("resNameInput").addEventListener("blur", (e) =>{
 
 // 수정 버튼
 
+document.getElementById("updateBtn").addEventListener("click", () =>{
+  document.getElementsByClassName("normal-container")[0].classList.add("b-hidden")
+  document.getElementsByClassName("edit-container")[0].classList.remove("b-hidden")
+})
+
+//종료 버튼
+
+document.getElementById("exitBtn").addEventListener("click", () =>{
+  document.getElementsByClassName("normal-container")[0].classList.remove("b-hidden")
+  document.getElementsByClassName("edit-container")[0].classList.add("b-hidden")
+})
+
+
+//추가 버튼
+
+document.getElementById("addMenu").addEventListener("click", () =>{
+  let x = document.getElementsByClassName("menu")
+  let name = Array.from(document.getElementsByClassName("menu-name"))
+  let price = Array.from(document.getElementsByClassName("menu-price"))
+
+  console.log(name)
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 삭제 버튼
+
+document.getElementById("deleteMenu").addEventListener("click", () =>{
+
+
+
+
+
+})
 
